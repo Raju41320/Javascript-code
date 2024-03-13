b9805191d204328fb02f7193b8e82c6c5d3aa57f
Raju Borkute <59973915+Raju41320@users.noreply.github.com>
2024-03-13 09:07:52 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "44anc"
+
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33asb" =>NaN
+// true => 1 , false => 0
+
+let isLoggedIn = "raju"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "raju" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
