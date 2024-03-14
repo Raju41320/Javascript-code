6d1c28978d4c4dd0fbbe577f3fad3c169aa6415b
Raju Borkute <59973915+Raju41320@users.noreply.github.com>
2024-03-14 19:03:36 +0000

    function operation

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,36 @@ function loginUserMessage (userName = "sam"){
 }
 
 // console.log(loginUserMessage("raju"));
-console.log(loginUserMessage("rajesh"));
+// console.log(loginUserMessage("rajesh"));
+
+function calculateCartPrice(val1,val2, ...num1) {
+    return num1;
+    
+}
+
+// console.log(calculateCartPrice(200,400,500,5000));
+
+const user = {
+    username : "raju",
+    price : 200
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+//handleObject(user);
+handleObject({
+    username : "raju",
+    price : 2333
+});
+
+const myNewArray = [200, 400, 509]
+
+function returnSecondValue(getArray) {
+    return  getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 509]));
