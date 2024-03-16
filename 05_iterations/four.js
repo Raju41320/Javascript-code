380a8664aac4850c87bb9a1a74d5dd2218f10fde
Raju Borkute <59973915+Raju41320@users.noreply.github.com>
2024-03-16 19:00:50 +0000

    for loops in js

---
+++ 05_iterations/four.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js: 'javascript',
+    cpp:'C++',
+    rb : 'ruby',
+    swift :'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(key);
+    // console.log(myObject[key]);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py","java", "cpp"]
+
+for (const key in programming) {
+    // console.log(key);
+    
+}
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United states America")
+// map.set('FR', "France")
+// map.set('UK', "United Kingdom")
+
+// for (const key in map) {
+//      console.log(key);
+// }
